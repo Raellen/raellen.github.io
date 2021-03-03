d34cdc73efb2736ffa52e565b59e5a64d5cac353
Raellen <akagi84345312@gmail.com>
2021-03-03 15:07:30 +0800

    vue-store

---
+++ vue-stores/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      name: 'Home',
+      path: '/index',
+      component: Home
+    },
+    // {
+    //   name: 'Home',
+    //   path: '/index',
+    //   component: Home
+    // }
+  ]
+})
